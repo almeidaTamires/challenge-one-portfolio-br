805a2494677249d49c14e76c17eeeb787b106062
Tamires <tamires_knowles@hotmail.com>
2022-06-19 22:28:06 -0300

    validate js

--- validate.js
+++ validate.js
@@ -1,1 +1,46 @@
 //Seu JavaScript de validação aqui
+const btn = document.getElementById("btn");
+
+    //validação do campo Nome
+    function validateForm() {
+        let inputNome = document.getElementById("nome").value;
+        if (inputNome === "") {
+            alert("O campo Nome deve ser preenchido")
+        }
+        if (inputNome.length > 50){
+            alert("Máximo de 50 caracteres no campo nome")
+        }
+    }
+    //validação do campo email
+    function validaEmail() {
+        let inputEmail = document.getElementById("email").value;
+        if (inputEmail === "") {
+            alert("O campo Email deve ser preenchido")
+        }
+    }
+    //validação do campo assunto
+    function validaAssunto() {
+        let inputAssunto = document.getElementById("assunto").value;
+        if (inputAssunto === "") {
+            alert("O campo Assunto deve ser preenchido")
+        }
+        if (inputAssunto.length > 50){
+            alert("Máximo de 50 caracteres no campo assunto")
+        }
+    }
+
+        //validação do campo mensagem
+        function validaMensagem() {
+            let inputMensagem = document.getElementById("mensagem").value;
+            if (inputMensagem === "") {
+                alert("O campo mensagem deve ser preenchido")
+            }
+            if (inputMensagem.length > 300){
+                alert("Máximo de 50 caracteres no campo mensagem")
+            }
+        }
+
+
+
+
+
